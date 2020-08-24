c817d60bdf761117ebcc74f333c08e580f86b1da
Chase Oliphant <chaseoliphant@gmail.com>
2020-08-23 19:31:50 -0700

    common

--- api/scripts/tests.sh
+++ api/scripts/tests.sh
@@ -1,5 +1,8 @@
 # mocha -r ts-node/register --timeout 999999 --colors --exit src/spec/**/*.spec.ts
 
+rm -rf spec-lib
+mkdir spec-lib
+
 cp package.json spec-lib/
 cp package-lock.json spec-lib/
 npm i --prefix spec-lib
