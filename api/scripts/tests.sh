3e6d6f56d173504601bbdeb1a90875be4ca6bb2f
Chase Oliphant <chaseoliphant@gmail.com>
2020-08-23 19:15:36 -0700

    wip

--- api/scripts/tests.sh
+++ api/scripts/tests.sh
@@ -1,10 +1,9 @@
 # mocha -r ts-node/register --timeout 999999 --colors --exit src/spec/**/*.spec.ts
 
-tsc -p ./tsconfig.spec.json
-
 cp package.json spec-lib/
 cp package-lock.json spec-lib/
-
 npm i --prefix spec-lib
+
+tsc -p ./tsconfig.spec.json
 
 mocha --timeout 999999  --exit --colors spec-lib/spec/**/*.spec.js
